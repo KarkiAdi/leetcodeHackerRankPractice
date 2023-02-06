6d9b8fd18e775da6176365b4ce81979a472d6410
KarkiAdi <adityakarki181@gmail.com>
2023-02-06 10:55:40 -0600

    Random Question - Print odd numbers in a given range

---
+++ PrintOddNumbers.java
@@ -0,0 +1,9 @@
+// print all odd numbers from 100 to 200
+public class PrintOddNumbers {
+    public static void main(String args[]) {
+        for (int i = 101; i <= 200; i += 2) {
+            System.out.println(i);
+        }
+    }
+
+}
